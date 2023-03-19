4cfcfb1813cf84f0bf4392b04da87f5f0afd28cd
John Connolly <jcconnol4@gmail.com>
2023-03-19 13:34:48 -0500

    add fishcam retrieval and formatting

--- components/fishCamera.js
+++ components/fishCamera.js
@@ -4,23 +4,35 @@ import styles from "../styles/fish-camera.module.css"
 export default function FishCamera() {
     const [tankTemp, setTankTemp] = useState(-1)
     const [imageUrl, setImageUrl] = useState("")
+    const [updateTimestamp, setUpdateTimestamp] = useState("")
     
     useEffect(() => {
-        setTimeout(() => {
-          console.log("whatever");
-          fetch("https://dkwxarv83g.execute-api.us-east-2.amazonaws.com/dev/api/fish-cam", { method: 'post'})
+        function getFishData() {
+            fetch("https://dkwxarv83g.execute-api.us-east-2.amazonaws.com/dev/api/fish-cam", { method: 'post'})
             .then(res => res.json())
             .then(
                 (result) => {
+                    let returnedTimestamp = new Date(result.last_run_time);
+                    let formattedDate = returnedTimestamp.toLocaleDateString() + ' ' + returnedTimestamp.toLocaleTimeString();
+                    setUpdateTimestamp(formattedDate)
                     setTankTemp(result.temp_f);
                     setImageUrl(result.s3_url);
                 },
                 (error) => {
+                    setUpdateTimestamp("None")
                     setTankTemp(-1);
                     setImageUrl("");
                 }
             )
-        }, 2000);
+        }
+
+        getFishData()
+
+        const interval = setInterval(() => getFishData(), 15000)
+
+        return () => {
+          clearInterval(interval);
+        }   
     }, []);
 
     return (
@@ -28,9 +40,12 @@ export default function FishCamera() {
             <div>
                 Tank Temp: {tankTemp}
             </div>
+            <div>
+                Last update: {updateTimestamp}
+            </div>
             {
                 imageUrl ?
-                <img src={imageUrl} />
+                <img src={imageUrl} className={styles["fish-image"]} />
                 :
                 "No image found!"
             }
